2cb7e557c3c3b81f310e6cd8857dbb788675dde3
Jesse <jesniphat@hotmail.com>
2018-11-04 20:55:11 +0700

    fix approuter

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -7,32 +7,32 @@ const Index = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
 const Users = () => <h2>Users</h2>;
 
-const AppRouter = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-          <li>
-            <Link to="/users/">Users</Link>
-          </li>
-          <li>
-            <Link to="/hello/">Hello</Link>
-          </li>
-        </ul>
-      </nav>
+export default function AppRouter() { 
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+            <li>
+              <Link to="/hello/">Hello</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Route path="/" exact component={Index} />
-      <Route path="/about/" component={About} />
-      <Route path="/users/" component={Users} />
-      <Route path='/hello/' component={Hello}/>
-    </div>
-  </Router>
-);
-
-export default AppRouter;
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+        <Route path='/hello/' component={Hello}/>
+      </div>
+    </Router>
+  );
+};
